89c3cb4ad68cf79f735b940546fb8c924a3e1807
BodaOsa <bodaosa@yahoo.com>
2021-06-27 16:23:02 +0800

    Finished the form layout (missing the intent)

--- app/src/main/java/com/example/covidapp/FormActivity.java
+++ app/src/main/java/com/example/covidapp/FormActivity.java
@@ -3,10 +3,14 @@ package com.example.covidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class FormActivity extends AppCompatActivity {
-    private TextView myTitle;
+    private TextView myTitle, myName, myAge, myPhone, myAddress, myEmail, myIC;
+    //private RadioGroup myGender;
     int listNumber;
 
     @Override
@@ -24,6 +28,61 @@ public class FormActivity extends AppCompatActivity {
         }
         else{
             myTitle.setText("Sinopharm User Form");
+        }
+        myName = findViewById(R.id.text_name);
+        //myGender = findViewById(R.id.radio_gender);
+        myAge = findViewById(R.id.text_age);
+        myPhone = findViewById(R.id.text_phone);
+        myAddress = findViewById(R.id.text_address);
+        myEmail = findViewById(R.id.text_email);
+        myIC = findViewById(R.id.text_ic);
+    }
+
+    public void confirm(View view) {
+        //Checks for a name
+        if (TextUtils.isEmpty(myName.getText())){
+            myName.setError("Please enter your name");
+        }
+        else{//Doesn't show error
+        }
+        //Checks for a age
+        if (TextUtils.isEmpty(myAge.getText())){
+            myAge.setError("Please enter your age");
+        }
+        else{//Doesn't show error
+        }
+        //Check for a number
+        if (TextUtils.isEmpty(myPhone.getText())){
+            myPhone.setError("Please enter your phone number");
+        }
+        else{//Doesn't show error
+        }
+        //Check for an address
+        if (TextUtils.isEmpty(myAddress.getText())){
+            myAddress.setError("Please enter your address");
+        }
+        else{//Doesn't show error
+        }
+        //Check for an email
+        if (TextUtils.isEmpty(myEmail.getText())){
+            myEmail.setError("Please enter your email address");
+        }
+        else{//Doesn't show error
+        }
+        //Check for an IC number
+        if (TextUtils.isEmpty(myIC.getText())){
+            myIC.setError("Please enter your IC number");
+        }
+        else{//Doesn't show error
+        }
+        //checks that all fields are filled
+        if(TextUtils.isEmpty(myName.getText()) == false && TextUtils.isEmpty(myAge.getText()) == false &&
+                TextUtils.isEmpty(myPhone.getText()) == false && TextUtils.isEmpty(myAddress.getText()) == false &&
+                TextUtils.isEmpty(myEmail.getText()) == false && TextUtils.isEmpty(myIC.getText()) == false){
+            //Proceeds to next activity
+        }
+        else {
+            //stays on the same page
         }
     }
 }
