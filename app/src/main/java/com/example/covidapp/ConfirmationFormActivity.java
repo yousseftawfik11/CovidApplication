efa5378bebb732aa8e136ccf7f1e800f012ce982
yousseftawfik11 <67921054+yousseftawfik11@users.noreply.github.com>
2021-06-27 12:36:40 +0200

    adding info to fields

--- app/src/main/java/com/example/covidapp/ConfirmationFormActivity.java
+++ app/src/main/java/com/example/covidapp/ConfirmationFormActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class ConfirmationFormActivity extends AppCompatActivity {
 
-    private TextView nameC,GenderC,myTitle;
+    private TextView nameC,GenderC,myTitle,phoneNumC,addressC,ageC,EmailC,ICnumC,First_date;
     int listNumber;
 
     @Override
@@ -30,8 +34,19 @@ public class ConfirmationFormActivity extends AppCompatActivity {
 
         nameC= findViewById(R.id.conf_name);
         GenderC= findViewById(R.id.conf_gender);
+        phoneNumC= findViewById(R.id.conf_phone);
+        addressC= findViewById(R.id.conf_address);
+        EmailC= findViewById(R.id.conf_email);
+        ICnumC= findViewById(R.id.conf_ICNum);
+        ageC= findViewById(R.id.conf_age);
+        First_date =findViewById(R.id.firstDose_date);
 
         String Pname= getIntent().getStringExtra("name");
+        String Pnum = getIntent().getStringExtra("phone");
+        String PAdress= getIntent().getStringExtra("address");
+        String PEmail= getIntent().getStringExtra("email");
+        String PICnum= getIntent().getStringExtra("icNumber");
+        String P_age= getIntent().getStringExtra("age");
         int genderNum= getIntent().getIntExtra("gender",0);
 
 
@@ -39,6 +54,25 @@ public class ConfirmationFormActivity extends AppCompatActivity {
 
         nameC.setText(Pname);
         GenderC.setText(genderNum);
+        phoneNumC.setText(Pnum);
+        addressC.setText(PAdress);
+        EmailC.setText(PEmail);
+        ICnumC.setText(PICnum);
+        ageC.setText(P_age);
+
+
+        Date currentDate = new Date();
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(currentDate);
+        c.add(Calendar.DAY_OF_MONTH, 1);
+
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String date = simpleDateFormat.format(new Date());
+
+        Date currentDatePlusOne = c.getTime();
+        First_date.setText(currentDatePlusOne.toString());
     }
 
 
