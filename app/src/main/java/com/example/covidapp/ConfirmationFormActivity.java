83b9fc3e3499b9a02188c3f333b0c15cbec11c94
yousseftawfik11 <67921054+yousseftawfik11@users.noreply.github.com>
2021-06-27 11:38:10 +0200

    sending info to confirmation progress

--- app/src/main/java/com/example/covidapp/ConfirmationFormActivity.java
+++ app/src/main/java/com/example/covidapp/ConfirmationFormActivity.java
@@ -3,12 +3,43 @@ package com.example.covidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConfirmationFormActivity extends AppCompatActivity {
+
+    private TextView nameC,GenderC,myTitle;
+    int listNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation_form);
+
+        myTitle= findViewById(R.id.vaccine_title);
+
+        listNumber = getIntent().getExtras().getInt("QuestionListNumber");
+        if (listNumber == 1){
+            myTitle.setText("Astrazeneca Confirmation Form");
+        }
+        else if(listNumber == 2){
+            myTitle.setText("Pfizer Confirmation Form");
+        }
+        else{
+            myTitle.setText("Sinopharm Confirmation Form");
+        }
+
+        nameC= findViewById(R.id.conf_name);
+        GenderC= findViewById(R.id.conf_gender);
+
+        String Pname= getIntent().getStringExtra("name");
+        int genderNum= getIntent().getIntExtra("gender",0);
+
+
+
+
+        nameC.setText(Pname);
+        GenderC.setText(genderNum);
     }
+
+
 }
