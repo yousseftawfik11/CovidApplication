1cb9820b1ccc60763916fb98c44324907ead8c04
BodaOsa <bodaosa@yahoo.com>
2021-07-16 03:23:26 +0800

    Login button done

--- app/src/main/java/com/example/covidapp/LoginPage.java
+++ app/src/main/java/com/example/covidapp/LoginPage.java
@@ -2,13 +2,44 @@ package com.example.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
+    DatabaseHelper dbHelper;
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        dbHelper = new DatabaseHelper(this);
+        username = findViewById(R.id.ETusername);
+        password = findViewById(R.id.ETpassword);
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void confirmLogin(View view) {
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
+
+        Boolean loggedIn = dbHelper.checkLogin(user, pass);
+        if(loggedIn){
+            displayToast("Login successful!");
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            displayToast("Invalid username or password!");
+            username.setText("");
+            password.setText("");
+        }
     }
 }
